855fe7ef2584a0f2404e8e311663f9ae5b2a87d5
Gareth Edwards <gareted@icloud.com>
2021-07-09 18:41:41 +0100

    first commit

---
+++ draw3d.cpp
@@ -0,0 +1,280 @@
+/*
+ * draw3d for Raspberry Pi Pico
+ *
+ * @version     1.0.0
+ * @author      Gareth Edwards
+ * @copyright   2021
+ * @licence     MIT
+ *
+ */
+#include "draw3d.h"
+#include <vector>
+#include <math.h>
+#include "time.h"
+#include "bsp/board.h"
+
+using namespace std;
+
+void drawLine(int x0, int y0, int x1, int y1);
+void setup(void);
+
+struct vec3d
+{
+    float x, y, z;
+};
+
+struct triangle
+{
+    vec3d p[3];
+};
+
+struct mesh
+{
+    vector<triangle> tris;
+};
+
+struct mat4x4
+{
+    float m[4][4] = {0};
+};
+
+mesh meshCube;
+mat4x4 matProj;
+
+uint32_t t1, t2;
+
+// rotation angle
+float fTheta;
+
+void MultiplyMatrixVector(vec3d &i, vec3d &o, mat4x4 &m)
+{
+    o.x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + m.m[3][0];
+    o.y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + m.m[3][1];
+    o.z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + m.m[3][2];
+    float w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + m.m[3][3];
+
+    if (w != 0.0f)
+    {
+        o.x /= w;
+        o.y /= w;
+        o.z /= w;
+    }
+}
+
+void DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+    drawLine(x1, y1, x2, y2);
+    drawLine(x2, y2, x3, y3);
+    drawLine(x3, y3, x1, y1);
+}
+
+int main()
+{
+    init_display(64, 4, 5);
+    //drawLine(10, 10, 110, 50);
+    float elapsedTime = 0.0f;
+    float fLastElapsed = 0.0f;
+    setup();
+    mat4x4 matRotZ, matRotX;
+
+    uint32_t time = board_millis();
+
+    t1 = board_millis();
+    t2 = board_millis();
+
+    while (1)
+    {
+        // Handle Timing
+        t1 = board_millis();
+
+		// Our time per frame coefficient
+		if (t2 - t1 < 50)
+        {
+            fTheta += 0.05f;
+            t2 = board_millis() + 50;
+        }
+
+
+        fill_scr(0);
+        //printf("fTheta: %f\n", fTheta);
+
+        // Rotation Z
+        matRotZ.m[0][0] = cosf(fTheta);
+        matRotZ.m[0][1] = sinf(fTheta);
+        matRotZ.m[1][0] = -sinf(fTheta);
+        matRotZ.m[1][1] = cosf(fTheta);
+        matRotZ.m[2][2] = 1;
+        matRotZ.m[3][3] = 1;
+
+        // Rotation X
+        matRotX.m[0][0] = 1;
+        matRotX.m[1][1] = cosf(fTheta * 0.5f);
+        matRotX.m[1][2] = sinf(fTheta * 0.5f);
+        matRotX.m[2][1] = -sinf(fTheta * 0.5f);
+        matRotX.m[2][2] = cosf(fTheta * 0.5f);
+        matRotX.m[3][3] = 1;
+
+        // Draw Triangles
+        for (auto tri : meshCube.tris)
+        {
+            triangle triProjected, triTranslated, triRotatedZ, triRotatedZX;
+
+            // Rotate in Z-Axis
+            MultiplyMatrixVector(tri.p[0], triRotatedZ.p[0], matRotZ);
+            MultiplyMatrixVector(tri.p[1], triRotatedZ.p[1], matRotZ);
+            MultiplyMatrixVector(tri.p[2], triRotatedZ.p[2], matRotZ);
+
+            // Rotate in X-Axis
+            MultiplyMatrixVector(triRotatedZ.p[0], triRotatedZX.p[0], matRotX);
+            MultiplyMatrixVector(triRotatedZ.p[1], triRotatedZX.p[1], matRotX);
+            MultiplyMatrixVector(triRotatedZ.p[2], triRotatedZX.p[2], matRotX);
+
+            // Offset into the screen
+            triTranslated = triRotatedZX;
+            triTranslated.p[0].z = triRotatedZX.p[0].z + 3.0f;
+            triTranslated.p[1].z = triRotatedZX.p[1].z + 3.0f;
+            triTranslated.p[2].z = triRotatedZX.p[2].z + 3.0f;
+
+            // Project triangles from 3D --> 2D
+            MultiplyMatrixVector(triTranslated.p[0], triProjected.p[0], matProj);
+            MultiplyMatrixVector(triTranslated.p[1], triProjected.p[1], matProj);
+            MultiplyMatrixVector(triTranslated.p[2], triProjected.p[2], matProj);
+
+            // Scale into view
+            triProjected.p[0].x += 1.0f;
+            triProjected.p[0].y += 1.0f;
+            triProjected.p[1].x += 1.0f;
+            triProjected.p[1].y += 1.0f;
+            triProjected.p[2].x += 1.0f;
+            triProjected.p[2].y += 1.0f;
+            triProjected.p[0].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[0].y *= 0.5f * (float)ScreenHeight();
+            triProjected.p[1].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[1].y *= 0.5f * (float)ScreenHeight();
+            triProjected.p[2].x *= 0.5f * (float)ScreenWidth();
+            triProjected.p[2].y *= 0.5f * (float)ScreenHeight();
+
+            // Rasterize triangle
+            DrawTriangle(triProjected.p[0].x, triProjected.p[0].y,
+                         triProjected.p[1].x, triProjected.p[1].y,
+                         triProjected.p[2].x, triProjected.p[2].y);
+        }
+
+        show_scr();
+    }
+    return 0;
+}
+
+void setup()
+{
+    meshCube.tris = {
+
+        // SOUTH
+        {0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f},
+
+        // EAST
+        {1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f},
+        {1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f},
+
+        // NORTH
+        {1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f},
+        {1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f},
+
+        // WEST
+        {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+
+        // TOP
+        {0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f},
+        {0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f},
+
+        // BOTTOM
+        {1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f},
+        {1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f},
+
+    };
+
+    // Projection Matrix
+    float fNear = 1.0f;
+    float fFar = 100.0f;
+    float fFov = 90.0f;
+    float fAspectRatio = (float)ScreenHeight() / (float)ScreenWidth();
+    float fFovRad = 1.0f / tanf(fFov * 0.5f / 180.0f * 3.14159f);
+
+    matProj.m[0][0] = fAspectRatio * fFovRad;
+    matProj.m[1][1] = fFovRad;
+    matProj.m[2][2] = fFar / (fFar - fNear);
+    matProj.m[3][2] = (-fFar * fNear) / (fFar - fNear);
+    matProj.m[2][3] = 1.0f;
+    matProj.m[3][3] = 0.0f;
+}
+
+//  https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+
+void plotLineLow(int x0, int y0, int x1, int y1)
+{
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int yi = 1;
+    if (dy < 0)
+    {
+        yi = -1;
+        dy = -dy;
+    }
+    int D = (2 * dy) - dx;
+    int y = y0;
+
+    for (int x = x0; x < x1 + 1; x++)
+    {
+        draw_pixel(x, y, 1);
+        if (D > 0)
+        {
+            y = y + yi;
+            D = D + (2 * (dy - dx));
+        }
+        else
+            D = D + 2 * dy;
+    }
+}
+
+void plotLineHigh(int x0, int y0, int x1, int y1)
+{
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+    int xi = 1;
+    if (dx < 0)
+    {
+        xi = -1;
+        dx = -dx;
+    }
+    int D = (2 * dx) - dy;
+    int x = x0;
+
+    for (int y = y0; y < y1 + 1; y++)
+    {
+        draw_pixel(x, y, 1);
+        if (D > 0)
+        {
+            x = x + xi;
+            D = D + (2 * (dx - dy));
+        }
+        else
+            D = D + 2 * dx;
+    }
+}
+
+void drawLine(int x0, int y0, int x1, int y1)
+{
+    if (abs(y1 - y0) < abs(x1 - x0))
+    {
+        if (x0 > x1)
+            plotLineLow(x1, y1, x0, y0);
+        else
+            plotLineLow(x0, y0, x1, y1);
+    }
+    else if (y0 > y1)
+        plotLineHigh(x1, y1, x0, y0);
+    else
+        plotLineHigh(x0, y0, x1, y1);
+}
---
+++ draw3d.h
@@ -0,0 +1,39 @@
+/*
+ * draw3d for Raspberry Pi Pico
+ *
+ * @version     1.0.0
+ * @author      Gareth Edwards
+ * @copyright   2021
+ * @licence     MIT
+ *
+ */
+#ifndef _DRAW3D_HEADER_
+#define _DRAW3D_HEADER_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
+
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
+#include "hardware/gpio.h"
+#include "hardware/i2c.h"
+#include "hardware/adc.h"
+#include "ssd1306.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Usual header code here
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _DRAW3D_HEADER_
